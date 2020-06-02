9928d2f90dc73a4996c7f30f59e3cec1f7862d32
kanarticho <eugenie.marine2@gmail.com>
2020-06-02 10:49:22 +0200

    quete05

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,16 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
+
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="^[a-z0-9-]+$"}, defaults={"slug"=1}, name="show")
+     */
+
+    public function show(?string $slug): Response
+    {
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
     }
 }
 
